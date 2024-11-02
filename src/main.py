f88a52915cc6cd491d8d666faad19a64bafc4e85
Shrouk Adel <140455175+Shrouk-Adel@users.noreply.github.com>
2024-11-02 14:43:50 +0200

    add percsion score (#3)

--- src/main.py
+++ src/main.py
@@ -1,6 +1,7 @@
 from sklearn.datasets import load_digits
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import classification_report,accuracy_score
+from sklearn.metrics import precision_score
 
 # load data from skleran
 digits = load_digits()
@@ -15,3 +16,6 @@ y_pred =model.predict(x)
 
 AC =accuracy_score(y,y_pred)
 print(f"Accuracy is {AC}")
+
+prec =precision_score(y,y_pred,average='macro')
+print(f"precession {prec}")
